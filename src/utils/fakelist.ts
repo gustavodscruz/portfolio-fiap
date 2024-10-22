39c93c1493fcb9b648c01a7efa1e3031f96cb4f1
vitorisacb <vitorisac14@gmail.com>
2024-10-22 08:58:45 -0300

    criação da lista tipada com as informações necessárias para incluir na tabela

--- src/utils/fakelist.ts
+++ src/utils/fakelist.ts
@@ -4,7 +4,11 @@ export type Tarefa = {
     materia : "Artificial Intelligence and Chatbot" | "Domain Driven Design Using Java" | "Computational Thinking Using Python" | "Software Engineering and Business Model" | "Building Relational Database" | "Front-End Design Engineering", 
     tipo : "Challenge" | "Global Solution" | "Checkpoints",
     descricao : string,
-    foto? : string 
+    foto? : string ,
+    semestre?: 1|2
+    nota: number,
+    feedback?: string
+
 }
 
 
@@ -14,105 +18,212 @@ export const tarefas : Tarefa[] = [
         titulo: "EDA - Análise Exploratória de Dados",
         descricao: "EDA, ou Análise Exploratória de Dados, é o processo inicial de análise de dados destinado a resumir suas principais características frequentemente usando métodos visuais. ",
         materia: "Artificial Intelligence and Chatbot",
-        tipo: "Challenge"
+        tipo: "Challenge",
+        nota: 0
     },
     {
         autor: "Felipe Ribeiro",
         titulo: "EDA - Análise Exploratória de Dados",
         descricao: "EDA, ou Análise Exploratória de Dados, é o processo inicial de análise de dados destinado a resumir suas principais características frequentemente usando métodos visuais. ",
         materia: "Artificial Intelligence and Chatbot",
-        tipo: "Challenge"
+        tipo: "Challenge",
+        nota: 0
     },
     {
         autor: "Júlia Angelozi",
         titulo: "EDA - Análise Exploratória de Dados",
         descricao: "EDA, ou Análise Exploratória de Dados, é o processo inicial de análise de dados destinado a resumir suas principais características frequentemente usando métodos visuais. ",
         materia: "Artificial Intelligence and Chatbot",
-        tipo: "Challenge"
+        tipo: "Challenge",
+        nota: 0
     },
     {
         autor: "Sofia Petruk",
         titulo: "EDA - Análise Exploratória de Dados",
         descricao: "EDA, ou Análise Exploratória de Dados, é o processo inicial de análise de dados destinado a resumir suas principais características frequentemente usando métodos visuais. ",
         materia: "Artificial Intelligence and Chatbot",
-        tipo: "Challenge"
+        tipo: "Challenge",
+        nota: 0
     },
     {
         autor: "Júlia Monteiro",
         titulo: "EDA - Análise Exploratória de Dados",
         descricao: "EDA, ou Análise Exploratória de Dados, é o processo inicial de análise de dados destinado a resumir suas principais características frequentemente usando métodos visuais. ",
         materia: "Artificial Intelligence and Chatbot",
-        tipo: "Challenge"
+        tipo: "Challenge",
+        nota: 0
     },
     {
         autor: "Gustavo Dias",
         titulo: "EDA - Análise Exploratória de Dados",
         descricao: "EDA, ou Análise Exploratória de Dados, é o processo inicial de análise de dados destinado a resumir suas principais características frequentemente usando métodos visuais. ",
         materia: "Artificial Intelligence and Chatbot",
-        tipo: "Checkpoints"
+        tipo: "Checkpoints",
+        nota: 0
     },
     {
         autor: "Felipe Ribeiro",
         titulo: "EDA - Análise Exploratória de Dados",
         descricao: "EDA, ou Análise Exploratória de Dados, é o processo inicial de análise de dados destinado a resumir suas principais características frequentemente usando métodos visuais. ",
         materia: "Artificial Intelligence and Chatbot",
-        tipo: "Checkpoints"
+        tipo: "Checkpoints",
+        nota: 0
     },
     {
         autor: "Júlia Angelozi",
         titulo: "EDA - Análise Exploratória de Dados",
         descricao: "EDA, ou Análise Exploratória de Dados, é o processo inicial de análise de dados destinado a resumir suas principais características frequentemente usando métodos visuais. ",
         materia: "Artificial Intelligence and Chatbot",
-        tipo: "Checkpoints"
+        tipo: "Checkpoints",
+        nota: 0
     },
     {
         autor: "Sofia Petruk",
         titulo: "EDA - Análise Exploratória de Dados",
         descricao: "EDA, ou Análise Exploratória de Dados, é o processo inicial de análise de dados destinado a resumir suas principais características frequentemente usando métodos visuais. ",
         materia: "Artificial Intelligence and Chatbot",
-        tipo: "Checkpoints"
+        tipo: "Checkpoints",
+        nota: 0
     },
     {
         autor: "Júlia Monteiro",
         titulo: "EDA - Análise Exploratória de Dados",
         descricao: "EDA, ou Análise Exploratória de Dados, é o processo inicial de análise de dados destinado a resumir suas principais características frequentemente usando métodos visuais. ",
         materia: "Artificial Intelligence and Chatbot",
-        tipo: "Checkpoints"
+        tipo: "Checkpoints",
+        nota: 0
     },
     {
         autor: "Gustavo Dias",
         titulo: "EDA - Análise Exploratória de Dados",
         descricao: "EDA, ou Análise Exploratória de Dados, é o processo inicial de análise de dados destinado a resumir suas principais características frequentemente usando métodos visuais. ",
         materia: "Artificial Intelligence and Chatbot",
-        tipo: "Global Solution"
+        tipo: "Global Solution",
+        nota: 0
     },
     {
         autor: "Felipe Ribeiro",
         titulo: "EDA - Análise Exploratória de Dados",
         descricao: "EDA, ou Análise Exploratória de Dados, é o processo inicial de análise de dados destinado a resumir suas principais características frequentemente usando métodos visuais. ",
         materia: "Artificial Intelligence and Chatbot",
-        tipo: "Global Solution"
+        tipo: "Global Solution",
+        nota: 0
     },
     {
         autor: "Júlia Angelozi",
         titulo: "EDA - Análise Exploratória de Dados",
         descricao: "EDA, ou Análise Exploratória de Dados, é o processo inicial de análise de dados destinado a resumir suas principais características frequentemente usando métodos visuais. ",
         materia: "Artificial Intelligence and Chatbot",
-        tipo: "Global Solution"
+        tipo: "Global Solution",
+        nota: 0
     },
     {
         autor: "Sofia Petruk",
         titulo: "EDA - Análise Exploratória de Dados",
         descricao: "EDA, ou Análise Exploratória de Dados, é o processo inicial de análise de dados destinado a resumir suas principais características frequentemente usando métodos visuais. ",
         materia: "Artificial Intelligence and Chatbot",
-        tipo: "Global Solution"
+        tipo: "Global Solution",
+        nota: 0
     },
     {
         autor: "Júlia Monteiro",
         titulo: "EDA - Análise Exploratória de Dados",
         descricao: "EDA, ou Análise Exploratória de Dados, é o processo inicial de análise de dados destinado a resumir suas principais características frequentemente usando métodos visuais. ",
         materia: "Artificial Intelligence and Chatbot",
-        tipo: "Global Solution"
+        tipo: "Global Solution",
+        nota: 0
     },
+   
+
+
+        {
+            autor: "Júlia Monteiro",
+            titulo: "CP1 - Entrega",
+            descricao: " Vários atributos estão sem comentários ou sem conteúdo significativo. A entidade de endereço do paciente carece de atributos, enquanto os da entidade de contatos estão incorretos. Também faltam regras de negócio para pacientes com vários telefones e e-mails.",
+            materia: "Building Relational Database",
+            tipo: "Checkpoints",
+            semestre: 1,
+            nota: 7
+        },
+        {
+            autor: "Júlia Monteiro",
+            titulo: "CP2 - Entrega",
+            descricao: "Nota: 9. Feedback: NULL.",
+            materia: "Building Relational Database",
+            tipo: "Checkpoints",
+            semestre: 1,
+            nota: 9
+            
+        },
+        {
+            autor: "Júlia Monteiro",
+            titulo: "CP3 - Entrega",
+            descricao: "Nota: . O nome 'Cadastro' deve ser substituído por algo mais significativo. As entidades CADASTRO, USUARIO_CADASTRO, USUARIO e LOGIN precisam ser revisadas, pois têm relacionamentos complexos. Também faltam entidades para 'incidente/interação'.",
+            materia: "Building Relational Database",
+            tipo: "Checkpoints",
+            semestre: 1,
+            nota: 6
+        },
+        {
+            autor: "Júlia Monteiro",
+            titulo: "Sprint1 - Entrega",
+            descricao: "Nota: . Estão usando atributos CHAR onde deveria ser VARCHAR. O relacionamento entre MECÂNICO e AVALIAÇÃO não precisa ser identificado.",
+            materia: "Building Relational Database",
+            tipo: "Challenge",
+            semestre: 1,
+            nota: 60
+        },
+        {
+            autor: "Júlia Monteiro",
+            titulo: "Sprint2 - Entrega",
+            descricao: "Nota: . As entidades LOGIN, USUARIO e CADASTRO são redundantes. A entidade OCORRENCIA precisa de mais atributos, e não há padrão de nomenclatura.",
+            materia: "Building Relational Database",
+            tipo: "Challenge",
+            semestre: 1,
+            nota: 100
+        },
+        {
+            autor: "Júlia Monteiro",
+            titulo: "Global Solution - 1º Semestre",
+            descricao: "Nota: . Feedback: NULL.",
+            materia: "Building Relational Database",
+            tipo: "Global Solution",
+            semestre: 1,
+            nota: 65
+        },
+        {
+            autor: "Júlia Monteiro",
+            titulo: "CP4 - Entrega",
+            descricao: " Feedback: NULL.",
+            materia: "Building Relational Database",
+            tipo: "Checkpoints",
+            semestre: 2,
+            nota: 8.6,
+            
+            
+        },
+        {
+            autor: "Júlia Monteiro",
+            titulo: "CP5 - Entrega",
+            descricao: "Nota: . Feedback: NULL.",
+            materia: "Building Relational Database",
+            tipo: "Checkpoints",
+            semestre: 2,
+            nota: 9.5
+        },
+        {
+            autor: "Júlia Monteiro",
+            titulo: "Sprint3 - Entrega",
+            descricao: " DDL 45 Nomeclatura 30 Relacional 20 Erro ao executar cria.",
+            materia: "Building Relational Database",
+            tipo: "Challenge",
+            semestre: 2,
+            nota: 95
+        }
+    ];
     
-] 
+    
+
+
+
+
