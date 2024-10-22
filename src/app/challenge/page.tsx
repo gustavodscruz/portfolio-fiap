b1e26bce54afdcda4727c4f7e1b590c13487da74
gustavodscruz <gustavodiasdsc@gmail.com>
2024-10-22 00:18:11 -0300

    hover botao verde

--- src/app/challenge/page.tsx
+++ src/app/challenge/page.tsx
@@ -1,9 +1,20 @@
 import Section from "@/components/MotionSection/section";
 import { tarefas } from "@/utils/fakelist";
 import Image from "next/image";
-import { FaFilter } from "react-icons/fa";
+import { useState } from "react";
+import {FaCheckSquare, FaFilter } from "react-icons/fa";
 
 export default function Challenge() {
+  const [hover, setHover] = useState(false)
+  
+  const handleMouseEnter = () => {
+    setHover(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHover(false);
+  };
+
   return (
     <div className="mt-8 flex flex-col gap-6">
       <Section>
@@ -37,16 +48,37 @@ export default function Challenge() {
               Como podemos ajudar o seu negócio
             </p>
           </div>
+          <div className="flex bg-[#011625] w-max p-3 rounded *:text-primary-text flex-col gap-1">
+            <div className="group flex p-2 font-primary tracking-wide text-sm gap-3 items-center hover:bg-[#022c4b] transition-all duration-300">
+                <p>Gustavo Dias</p>
+                <FaCheckSquare color="#a3a4a8" size={15} className=" group-hover:custom-filter-green transition-colors duration-300" style={{transform: hover ? "brightness(0) saturate(100%) invert(54%) sepia(77%) saturate(3778%) hue-rotate(112deg) brightness(90%) contrast(83%)" : ''}} 
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                />
+            </div>
+            <div className="flex p-2 font-primary tracking-wide text-sm">
+                <p>Sofia Petruk</p>
+            </div>
+            <div className="flex p-2 font-primary tracking-wide text-sm">
+                <p>Felipe Ribeiro</p>
+            </div>
+            <div className="flex p-2 font-primary tracking-wide text-sm">
+                <p>Júlia Monteiro</p>
+            </div>
+            <div className="flex p-2 font-primary tracking-wide text-sm">
+                <p>Júlia Angelozi</p>
+            </div>
+          </div>
           <table className="w-full mt-8">
             <thead className="bg-[#011625]">
                 <tr className="">
-                    <th className="flex p-5 items-center gap-3"><p className="text-primary-text font-primary text-lg">Autor</p> <FaFilter color="#FFF" size={15} className="hover:scale-125 hover:brightness-0 hover:saturate-100 hover:invert-[50%] cursor-pointer transition-all duration-300" /></th>
+                    <th className=" flex p-5 items-center gap-3"><p className="text-primary-text font-primary text-lg">Autor</p> <FaFilter color="#FFF" size={15} className="hover:scale-125 hover:brightness-0 hover:saturate-100 hover:invert-[50%] cursor-pointer transition-all duration-300" /></th>
                     <th className="p-5 items-center gap-3"><p className="text-primary-text font-primary text-lg">Tarefa</p></th>
                     <th className="p-5 items-center gap-3"><p className="text-primary-text font-primary text-lg">Matéria</p></th>
-                    <th className="p-5 items-center gap-3"><p className="text-primary-text font-primary text-lg">Tipo</p></th>
+                    <th className="rounded-tr-md p-5 items-center gap-3"><p className="text-primary-text font-primary text-lg">Tipo</p></th>
                 </tr>
             </thead>
-            <tbody className="bg-[#001d31] ">
+            <tbody className="bg-[#001d31] last:rounded-b-md">
                 {tarefas.map((tarefa, indice)=>(
                     tarefa.tipo.includes("Challenge") ? 
                     (<tr key={indice}>
