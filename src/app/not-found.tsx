c0b8950067913a536e947574464c496e0ed18ef9
jliamonteiro <jlmonteiro2355@gmail.com>
2024-10-23 00:02:18 -0300

    finalização da página de erro com imagem inserida

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,16 +4,19 @@ import Image from 'next/image';
 
 export default function PageNotFound() {
   return (
-    <div className='place-items-center'>
-        <h1 className='text-red-custom font-bold text-6xl'> 404</h1>
-        <h2 className='text-primary-text font-semibold'>Ops! Conteúdo não localizado!</h2>
-        <Image
-              src="/erro.jpg"
-              alt="Como a IA pode ajudar a solucionar problemas?"
-              height={100}
-              width={600}
-              
-            />
+    <div className='flex flex-col items-center'>
+      <h1 className='text-red-custom font-bold text-6xl mb-2'>404</h1> 
+      <h2 className='text-primary-text font-semibold mb-7'>Ops! Conteúdo não localizado!</h2> 
+      <Image
+          src="/morreu.gif"
+          alt="Como a IA pode ajudar a solucionar problemas?"
+          height={100}
+          width={600}
+          className='object-contain border-4 border-red-custom rounded-md bg-red-100 max-w-1/2 m-auto rounded-lg'
+      />
     </div>
+
+
+
   )
 }
