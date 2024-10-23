cfc3a53cb5e7b05bd6b54c9abd55c5809c0a1636
jliamonteiro <jlmonteiro2355@gmail.com>
2024-10-22 23:33:48 -0300

    estilização da página de erro

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-export default function NotFound() {
+import Image from 'next/image';
+
+export default function PageNotFound() {
   return (
-    <div>
-        <h1>NOT FOUND - 404</h1>
-        <h2>Página não encontrada...</h2>
+    <div className='place-items-center'>
+        <h1 className='text-red-custom font-bold text-6xl'> 404</h1>
+        <h2 className='text-primary-text font-semibold'>Ops! Conteúdo não localizado!</h2>
+        <Image
+              src="/erro.jpg"
+              alt="Como a IA pode ajudar a solucionar problemas?"
+              height={100}
+              width={600}
+              
+            />
     </div>
   )
 }
