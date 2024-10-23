402130a112c044037120b8ce3bb58fd8e1f6c154
jliamonteiro <jlmonteiro2355@gmail.com>
2024-10-22 22:22:24 -0300

    criando a página not-found (erro)

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>
+        <h1>NOT FOUND - 404</h1>
+        <h2>Página não encontrada...</h2>
+    </div>
+  )
+}
